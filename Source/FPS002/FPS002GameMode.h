08ec23aee16f7054084425ddf26ec66a3d174aa0
Julien Folliot <1753578+JulienFolliot@users.noreply.github.com>
2023-03-16 21:52:18 +0100

    WIP Restart

--- Source/FPS002/FPS002GameMode.h
+++ Source/FPS002/FPS002GameMode.h
@@ -3,11 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameModeBase.h"
+#include "GameFramework/GameMode.h"
 #include "FPS002GameMode.generated.h"
 
 UCLASS(minimalapi)
-class AFPS002GameMode : public AGameModeBase
+class AFPS002GameMode : public AGameMode
 {
 	GENERATED_BODY()
 
